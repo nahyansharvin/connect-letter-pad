a0248d1f834cb728d533cea5e68b95558a73c308
Nahyan Sharvin <nskonnalath@gmail.com>
2022-09-24 11:44:33 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar'
 import AddBody from './pages/addBodyDetails/AddBody';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <AddBody />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} >
+           <Route index element={<AddBody />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
